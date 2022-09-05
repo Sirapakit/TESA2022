9583ec15a274c31273a36c7e09a1cd475722f390
Pupae <noparin.sme@student.mahidol.edu>
2022-09-05 19:55:01 +0700

    Update function

--- functions/connect_mqtt.m
+++ functions/connect_mqtt.m
@@ -2,11 +2,14 @@ function connect_mqtt()
     global Address mqClient data 
 
     % connect to mqtt %
-    Address = "tcp://broker.hivemq.com";  % tcp://10.13.8.163
+    %Address = "tcp://broker.hivemq.com";  % tcp://10.13.8.163
+    Address = "tcp://10.13.8.163";
     Topic = "TEST/TESA";
     mqClient = mqttclient(Address);
     subscribe(mqClient,Topic);
+    
     disp("MQTT connected");
     data = read(mqClient)
+    pause(.5)
 
 end
